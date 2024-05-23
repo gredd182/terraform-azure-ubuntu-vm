ebed003c9ae1be72859f72144f1b0c22f658a118
gredd182 <134130676+gredd182@users.noreply.github.com>
2024-05-23 11:28:12 +0530

    Update network-main.tf

--- network-main.tf
+++ network-main.tf
@@ -4,30 +4,22 @@
 
 # Create a resource group for network
 resource "azurerm_resource_group" "network-rg" {
-  name     = "linux-${lower(replace(var.app_name," ","-"))}-${var.environment}-rg"
+  name     = "network-rg"
   location = var.location
-  tags = {
-    application = var.app_name
-    environment = var.environment
-  }
 }
 
 # Create the network VNET
 resource "azurerm_virtual_network" "network-vnet" {
-  name                = "linux-${lower(replace(var.app_name," ","-"))}-${var.environment}-vnet"
+  name                = "network-vnet"
   address_space       = [var.network-vnet-cidr]
   resource_group_name = azurerm_resource_group.network-rg.name
   location            = azurerm_resource_group.network-rg.location
-  tags = {
-    application = var.app_name
-    environment = var.environment
-  }
 }
 
-# Create a subnet for Network
-resource "azurerm_subnet" "network-subnet" {
-  name                 = "linux-${lower(replace(var.app_name," ","-"))}-${var.environment}-subnet"
-  address_prefixes     = [var.network-subnet-cidr]
+# Create a subnet for vm
+resource "azurerm_subnet" "vm-subnet" {
+  name                 = "vm-subnet"
+  address_prefixes     = [var.vm-subnet-cidr]
   virtual_network_name = azurerm_virtual_network.network-vnet.name
   resource_group_name  = azurerm_resource_group.network-rg.name
 }
